d1734f571a4799296898718befa23e349ae45c5a
ᒋ <gamer0968.bh@gmail.com>
2025-11-10 09:00:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ app = gr.Interface(
     examples=[["th.jpg"]]
 )
 st.title("")
-st.write('<iframe src="https://8bd58af76f58dd5227.gradio.live" width="800" height="600"></iframe>',
+st.write('<iframe src="https://8bd58af76f58dd5227.gradio.live" width="1000" height="750"></iframe>',
          unsafe_allow_html=True)
 if __name__ == "__main__":
     app.launch(server_name="0.0.0.0", share = True)
